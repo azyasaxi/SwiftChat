d30c782660c0c32167419bc59f3604aa338793bc
azyasaxi <2788243016@qq.com>
2025-02-24 21:53:23 +0800

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Azyasaxi on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chat
+//
+//  Created by Azyasaxi on 2/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
